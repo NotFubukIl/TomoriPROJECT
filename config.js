c20575b18479b26ce8ce0c4001c87483cdd8a651
GayarraFrost <75570490+GayarraFrost@users.noreply.github.com>
2021-05-16 21:35:17 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,89 @@
+const fs = require('fs')
+const path = "./node_modules/tomoriproject/config.js"
+/*
+_________                _____.__                            __  .__               
+\_   ___ \  ____   _____/ ____\__| ____  __ ______________ _/  |_|__| ____   ____  
+/    \  \/ /  _ \ /    \   __\|  |/ ___\|  |  \_  __ \__  \\   __\  |/  _ \ /    \ 
+\     \___(  <_> )   |  \  |  |  / /_/  >  |  /|  | \// __ \|  | |  (  <_> )   |  \
+ \______  /\____/|___|  /__|  |__\___  /|____/ |__|  (____  /__| |__|\____/|___|  /
+        \/            \/        /_____/                   \/                    \/
+*/
+fs.writeFile(path, `
+
+const token = "NjI2MTY1MjQwMDU2MjUwMzY5.YJ5AQA.jQdCGVJXGbgqqP4dmPyPzevOu54"// Mets ton token entre les ""
+const prefix = "&"// Mets ton prerfx entre les "'
+const image = ""// Mets le lien de ton image entre les "" (.gif, .png, .jpg ...)
+const autoclaimer = "on" // on ou off (on : Activer, off : Désactiver)
+const color = "red" // Mets ta couleur exemple : RANDOM/red/blue Ou des couleurs HTML exemple : #00aaaa
+const twitch = "" // Mettez votre lien twitch ici (Facultatif)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+	token,
+	prefix,
+	image,
+	autoclaimer,
+	color,
+	twitch
+}
+`, err => {
+	if (err) console.error(err);
+});
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+cls
+node config.js
+node ./node_modules/tomoriproject/index.js
+PAUSE
