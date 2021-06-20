4a060ca4c084241a188f6cf2991d58eeb5802c92
GayarraFrost <75570490+GayarraFrost@users.noreply.github.com>
2021-06-20 15:09:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ _________                _____.__                            __  .__
         \/            \/        /_____/                   \/                    \/
 */
 
-const token = "NzE1MTY2OTQ3NjI4ODEwMjUw.YMevHA.xCpfnwOOaVkNDD6sXz4wKP6t_iY" // Mets ton token entre les ""
-const prefix = "&"// Mets ton prefix entre les ""
-const image = "https://images-ext-1.discordapp.net/external/zpp-Ds9dIFpOf1B7XiSZyHxQxGgmdnPm6wkSDezKukk/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/276357866217013249/713e43988ed751cf87501c8b5a5b7dcc.png?width=473&height=473"// Mets le lien de ton image entre les "" (.gif, .png, .jpg ...)
-const autoclaimer = "on" // on ou off (on : Activer, off : Désactiver)
-const color = "#00aaaa" // Mets ta couleur exemple : RANDOM/red/blue Ou des couleurs HTML exemple : #00aaaa
+const token = "" // Mets ton token entre les ""
+const prefix = ""// Mets ton prefix entre les ""
+const image = ""// Mets le lien de ton image entre les "" (.gif, .png, .jpg ...)
+const autoclaimer = "" // on ou off (on : Activer, off : Désactiver)
+const color = "" // Mets ta couleur exemple : RANDOM/red/blue Ou des couleurs HTML exemple : #00aaaa
 const twitch = "" // Mettez votre lien twitch ici (Facultatif)
-const rpc = "off" // ON OU OFF POUR DESACTIVER LE RPC DE BASE DU PROJECT
-const consolecolor = "green" // Mettez la couleurs que vous voulez (plus bas) Si aucune couleur n'est définit, la console sera verte
-const antipubbot = "on" // on ou off pour activer ou désactiver les pubs des bots
+const rpc = "" // ON OU OFF POUR DESACTIVER LE RPC DE BASE DU PROJECT
+const consolecolor = "" // Mettez la couleurs que vous voulez (plus bas) Si aucune couleur n'est définit, la console sera verte
+const antipubbot = "" // on ou off pour activer ou désactiver les pubs des bots
 const statut = ["", "", "", "", "", ""] // Statut qui change :0 (si vous faites la commandes ^^) si vous voulez en mettre plus vous faites ,"" , ""
 
 /*
