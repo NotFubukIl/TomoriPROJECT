e766402121d9b624276588e4bc2995d722d21798
GayarraFrost <75570490+GayarraFrost@users.noreply.github.com>
2021-06-30 13:24:49 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -6,28 +6,32 @@ _________                _____.__                            __  .__
  \______  /\____/|___|  /__|  |__\___  /|____/ |__|  (____  /__| |__|\____/|___|  /
         \/            \/        /_____/                   \/                    \/
 */
-
-const token = "" // Mets ton token entre les ""
-const prefix = ""// Mets ton prefix entre les ""
-const image = ""// Mets le lien de ton image entre les "" (.gif, .png, .jpg ...)
-const autoclaimer = "" // on ou off (on : Activer, off : Désactiver)
-const color = "" // Mets ta couleur exemple : RANDOM/red/blue Ou des couleurs HTML exemple : #00aaaa
+const token = "mfa.nQanwAf-xzBNNfXP6iLbw7LkisKXNcwRz9zWIYH1pDQ70NUPXrkxOLTh587A--kPXCdRT2tddvhiAWsbxuWO" // Mets ton token entre les ""
+const prefix = "&"// Mets ton prefix entre les ""
+const image = "https://images-ext-2.discordapp.net/external/OsOEypzd9XltsfEpxdnsh0j_0cbYOZQr8Dz5wXto2_0/https/cdn.mee6.xyz/guild-images/842019518767562772/eee1e940827e9b58c79aa38bd6f325587470ba1573fa530889c724b780ba0ad5.gif"// Mets le lien de ton image entre les "" (.gif, .png, .jpg ...)
+const autoclaimer = "on" // on ou off (on : Activer, off : Désactiver)
+const color = "#00aaaa" // Mets ta couleur exemple : RANDOM/red/blue Ou des couleurs HTML exemple : #00aaaa
 const twitch = "" // Mettez votre lien twitch ici (Facultatif)
-const rpc = "" // ON OU OFF POUR DESACTIVER LE RPC DE BASE DU PROJECT
-const consolecolor = "" // Mettez la couleurs que vous voulez (plus bas) Si aucune couleur n'est définit, la console sera verte
-const antipubbot = "" // on ou off pour activer ou désactiver les pubs des bots
-const statut = ["", "", "", ""] // Statut qui change :0 (si vous faites la commandes ^^) si vous voulez en mettre plus vous faites ,"" , ""
-
+const rpc = "off" // ON OU OFF POUR DESACTIVER LE RPC DE BASE DU PROJECT
+const consolecolor = "rainbow" // Mettez la couleurs que vous voulez (plus bas) Si aucune couleur n'est définit, la console sera verte
+const antipubbot = "on" // on ou off pour activer ou désactiver les pubs des bots
+const statut = ["Coucou", "Ma Baka A moaaa", "Goulag", "hehe"] // Statut qui change :0 (si vous faites la commandes ^^) si vous voulez en mettre plus vous faites ,"" , ""
 /*
-
 White,
 green,
 blue,
 rainbow,
 magenta,
 yellow
-
 */
+// RPC CUSTOM lien = https://discord.com/developers/applications
+const appid = "834031785528131594" // Mettez L'ID De Votre Applications Discord Pour Le RPC Custom
+const nomimage = "gungirl" // Mettez Le Nom De Votre Grande Image ICI
+const titrerpc = "Cc" // Mettez Le Titre De Votre RPC (Joue a ...)
+const details = "cv" // Mettez Le 2e texte qui apparaitra sur le RPC
+const state = "sltcv" // Ici tu mets le 3e texte qui apparaitre sur ton RPC
+const textegrandeimage = "gun" // Ici tu mets le texte qui apparaitra sur ton RPC quand on mettras la souris sur la grande Image
+const textepetiteimage = "tropdark" // Ici tu mets le texte qui apparaitre sur ton RPC quand on mettras la souris sur la petite Image
 
 
 
@@ -98,5 +102,12 @@ module.exports = {
 	rpc,
 	consolecolor,
 	antipubbot,
-	statut
+	statut,
+	appid,
+	nomimage,
+	details,
+	titrerpc,
+	state,
+	textegrandeimage,
+	textepetiteimage
 }
