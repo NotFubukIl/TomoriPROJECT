ca1b3a5d37bc42e2600b07f6ec9e737059b7163d
GayarraFrost <75570490+GayarraFrost@users.noreply.github.com>
2021-07-11 20:49:07 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,38 +1,37 @@
-/*
-_________                _____.__                            __  .__               
-\_   ___ \  ____   _____/ ____\__| ____  __ ______________ _/  |_|__| ____   ____  
-/    \  \/ /  _ \ /    \   __\|  |/ ___\|  |  \_  __ \__  \\   __\  |/  _ \ /    \ 
-\     \___(  <_> )   |  \  |  |  / /_/  >  |  /|  | \// __ \|  | |  (  <_> )   |  \
- \______  /\____/|___|  /__|  |__\___  /|____/ |__|  (____  /__| |__|\____/|___|  /
-        \/            \/        /_____/                   \/                    \/
-*/
-const token = "" // Mets ton token entre les ""
-const prefix = ""// Mets ton prefix entre les ""
-const image = ""// Mets le lien de ton image entre les "" (.gif, .png, .jpg ...)
-const autoclaimer = "" // on ou off (on : Activer, off : Désactiver)
-const color = "" // Mets ta couleur exemple : RANDOM/red/blue Ou des couleurs HTML exemple : #00aaaa
-const twitch = "" // Mettez votre lien twitch ici (Facultatif)
-const rpc = "" // ON OU OFF POUR DESACTIVER LE RPC DE BASE DU PROJECT
-const consolecolor = "" // Mettez la couleurs que vous voulez (plus bas) Si aucune couleur n'est définit, la console sera verte
-const antipubbot = "" // on ou off pour activer ou désactiver les pubs des bots
-const statut = ["", "", "", ""] // Statut qui change :0 (si vous faites la commandes ^^) si vous voulez en mettre plus vous faites ,"" , ""
-/*
-White,
-green,
-blue,
-rainbow,
-magenta,
-yellow
-*/
-// RPC CUSTOM lien = https://discord.com/developers/applications
-const appid = "" // Mettez L'ID De Votre Applications Discord Pour Le RPC Custom
-const nomimage = "" // Mettez Le Nom De Votre Grande Image ICI
-const titrerpc = "" // Mettez Le Titre De Votre RPC (Joue a ...)
-const details = "" // Mettez Le 2e texte qui apparaitra sur le RPC
-const state = "" // Ici tu mets le 3e texte qui apparaitre sur ton RPC
-const textegrandeimage = "" // Ici tu mets le texte qui apparaitra sur ton RPC quand on mettras la souris sur la grande Image
-const textepetiteimage = "" // Ici tu mets le texte qui apparaitre sur ton RPC quand on mettras la souris sur la petite Image
 
+        /*
+        _________                _____.__                            __  .__               
+        _   ___   ____   _____/ ______| ____  __ ______________ _/  |_|__| ____   ____  
+        /      / /  _  /       __|  |/ ___|  |  _  __ __  \   __  |/  _  /     
+             ___(  <_> )   |    |  |  / /_/  >  |  /|  | // __ |  | |  (  <_> )   |           ______  /____/|___|  /__|  |_____  /|____/ |__|  (____  /__| |__|____/|___|  /
+                /            /        /_____/                   /                    /
+        */
+    const token = "" // Mets ton token entre les ""
+    const prefix = ""// Mets ton prefix entre les ""
+    const image = ""// Mets le lien de ton image entre les "" (.gif, .png, .jpg ...)
+    const autoclaimer = "" // on ou off (on : Activer, off : Désactiver)
+    const color = "" // Mets ta couleur exemple : RANDOM/red/blue Ou des couleurs HTML exemple : #00aaaa
+    const twitch = "" // Mettez votre lien twitch ici (Facultatif)
+    const rpc = "" // ON OU OFF POUR DESACTIVER LE RPC DE BASE DU PROJECT
+    const consolecolor = "" // Mettez la couleurs que vous voulez (plus bas) Si aucune couleur n'est définit, la console sera verte
+    const antipubbot = "" // on ou off pour activer ou désactiver les pubs des bots
+    const statut = ["", "", "", ""] // Statut qui change :0 (si vous faites la commandes ^^) si vous voulez en mettre plus vous faites ,"" , ""
+    /*
+    White,
+    green,
+    blue,
+    rainbow,
+    magenta,
+    yellow
+    */
+    // RPC CUSTOM lien = https://discord.com/developers/applications
+    const appid = "" // Mettez L'ID De Votre Applications Discord Pour Le RPC Custom
+    const nomimage = "" // Mettez Le Nom De Votre Grande Image ICI
+    const titrerpc = "" // Mettez Le Titre De Votre RPC (Joue a ...)
+    const details = "" // Mettez Le 2e texte qui apparaitra sur le RPC
+    const state = "" // Ici tu mets le 3e texte qui apparaitre sur ton RPC
+    const textegrandeimage = "" // Ici tu mets le texte qui apparaitra sur ton RPC quand on mettras la souris sur la grande Image
+    const textepetiteimage = "" // Ici tu mets le texte qui apparaitre sur ton RPC quand on mettras la souris sur la petite Image
     /*        
             
             
