49f68c08a878f8ae2b6ba1cff256ad7d3e7ad210
GayarraFrost <75570490+GayarraFrost@users.noreply.github.com>
2021-07-30 19:28:52 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -5,26 +5,24 @@
         \     \___(  <_> )   |  \  |  |  / /_/  >  |  /|  | \// __ \|  | |  (  <_> )   |  \
          \______  /\____/|___|  /__|  |__\___  /|____/ |__|  (____  /__| |__|\____/|___|  /
                 \/            \/        /_____/                   \/                    \/
+                
 */
-    const token = "" // Mets ton token entre les ""
-    const prefix = ""// Mets ton prefix entre les ""
-    const image = ""// Mets le lien de ton image entre les "" (.gif, .png, .jpg ...)
-    const autoclaimer = "" // on ou off (on : Activer, off : Désactiver)
-    const color = "" // Mets ta couleur exemple : RANDOM/red/blue Ou des couleurs HTML exemple : #00aaaa
-    const twitch = "" // Mettez votre lien twitch ici (Facultatif)
-    const rpc = "" // ON OU OFF POUR DESACTIVER LE RPC DE BASE DU PROJECT
-    const consolecolor = "" // Mettez la couleurs que vous voulez (plus bas) Si aucune couleur n'est définit, la console sera verte
-    const antipubbot = "" // on ou off pour activer ou désactiver les pubs des bots
-    const statut = ["", "", "", ""] // Statut qui change :0 (si vous faites la commandes ^^) si vous voulez en mettre plus vous faites ,"" , ""
-    /*
-    White,
-    green,
-    blue,
-    rainbow,
-    magenta,
-    yellow
-    */
-    // RPC CUSTOM lien = https://discord.com/developers/applications
+const token = "" // Mets ton token entre les ""
+const prefix = "" // Mets ton prefix entre les ""
+const image = "" // Mets le lien de ton image entre les "" (.gif, .png, .jpg ...)
+const autoclaimer = "" // on ou off (on : Activer, off : Désactiver)
+const color = "" // Mets ta couleur exemple : RANDOM/red/blue Ou des couleurs HTML exemple : #00aaaa
+const webhook = "" // Mettez Le Lien D'Un Webhook Ici, facultatif mais envoi le nitro snipe sur ce webhook
+const twitch = "" // Mettez votre lien twitch ici (Facultatif)
+const rpc = "" // ON OU OFF POUR DESACTIVER LE RPC DE BASE DU PROJECT
+const consolecolor = "" // Mettez la couleurs que vous voulez (plus bas) Si aucune couleur n'est définit, la console sera verte
+const antipubbot = "" // on ou off pour activer ou désactiver les pubs des bots
+const statut = ["", "", "", ""] // Statut qui change :0 (si vous faites la commandes ^^) si vous voulez en mettre plus vous faites ,"" , ""
+const guildsnipe = ["", "", "", ""] // Mettez l'ID Des Server ou vous voulez que les messages supprimé sois envoyé au channel demander plus bas
+const snipelog = "" // Mettez l'ID Du channel où vous voulez que les messages supprimé de guildsnipe s'envoie
+const premium = "" // Mettez Votre Clé Premium ICI.
+// White, green, blue, rainbow, magenta, yellow
+// RPC CUSTOM lien = https://discord.com/developers/applications
 const appid = "" // Mettez L'ID De Votre Applications Discord Pour Le RPC Custom
 const nomimage = "" // Mettez Le Nom De Votre Grande Image ICI
 const nompetiteimage = "" // Mettez Ici Le Nom De Votre Petite Image
@@ -33,77 +31,51 @@ const details = "" // Mettez Le 2e texte qui apparaitra sur le RPC
 const state = "" // Ici tu mets le 3e texte qui apparaitre sur ton RPC
 const textegrandeimage = "" // Ici tu mets le texte qui apparaitra sur ton RPC quand on mettras la souris sur la grande Image
 const textepetiteimage = "" // Ici tu mets le texte qui apparaitre sur ton RPC quand on mettras la souris sur la petite Image
-/*        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-*/
+// RPC SPOTIFY CUSTOM 
+const imgname = "" // Mettez Le Nom De L'Image De Votre Spotidy Custom
+const littleimgname = "" // Mettez Le Nom De Votre Petite Image Spotify
+const detail = "" // Mettez Ce Que Vous Voulez Ecouter.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// NE PAS TOUCHER OR GAY.
 module.exports = {
         token,
         prefix,
         image,
         autoclaimer,
         color,
+        webhook,
         twitch,
         rpc,
         consolecolor,
         antipubbot,
+        guildsnipe,
+        snipelog,
         statut,
+        premium,
         appid,
         nomimage,
         nompetiteimage,
@@ -111,6 +83,9 @@ module.exports = {
         titrerpc,
         state,
         textegrandeimage,
-        textepetiteimage
+        textepetiteimage,
+        imgname,
+        littleimgname,
+        detail
 }
-            
+// NE PAS TOUCHER OR GAY.
